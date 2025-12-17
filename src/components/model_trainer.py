27e961f05ae1fa9c1cf7e3e930b1cbbb6bbf4049
Sammarth <sammarth18@gmail.com>
2025-12-17 19:51:37 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -45,6 +45,37 @@ class ModelTrainer:
                 "CatBoost Regressor":CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor":AdaBoostRegressor(),
             }
+            ## Hyperparameters for each model
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [50, 100, 200],
+                    'max_features': ['auto', 'sqrt', 'log2'],
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.9],
+                    'n_estimators':[50,100,200],
+                },
+                "Linear Regression":{},
+                "XGB Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[50,100,200],
+                },
+                "CatBoost Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'iterations':[30,50,100],
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[50,100,200],
+                },
+                       
+            }
+
 
             model_report={}
 
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import dill
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -16,4 +18,29 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(str(e), sys)
-    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv=5, n_jobs=-1, verbose=1, refit=True)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    except Exception as e:
+        raise CustomException(str(e), sys)
